3088ed7a6dee7b922b845e20fde55a3f87243a8a
jxp000000111 <60886183+jxp000000111@users.noreply.github.com>
2026-03-24 16:55:47 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ccyuodbeezbvprhgjpko.supabase.co";
-const supabaseAnonKey = "sb_publishable_ud_KIbWXA3W9HLIFPpur7g_Ir1_oOWZ";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+);
